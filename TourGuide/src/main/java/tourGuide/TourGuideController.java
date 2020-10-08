88feb782adf690abf04ec6d59c6a339755d6c21d
olivier.bonheur <olivier.bonheur@gmail.com>
2020-10-08 12:02:01 +0200

    WOrk on TourGuideController

--- TourGuide/src/main/java/tourGuide/TourGuideController.java
+++ TourGuide/src/main/java/tourGuide/TourGuideController.java
@@ -5,10 +5,7 @@ import java.util.List;
 
 import gpsUtil.location.Location;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import gpsUtil.location.VisitedLocation;
 import tourGuide.attraction.NearbyAttraction;
@@ -24,47 +21,47 @@ public class TourGuideController {
 	@Autowired
 	TourGuideService tourGuideService;
 	
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "Greetings from TourGuide!";
     }
     
-    @RequestMapping("/getLocation") 
+    @GetMapping("/getLocation")
     public Location getLocation(@RequestParam String userName) {
     	VisitedLocation visitedLocation = tourGuideService.getUserLocation(getUser(userName));
 		return visitedLocation.location;
     }
     
-    @RequestMapping("/getNearbyAttractions")
+    @GetMapping("/getNearbyAttractions")
     public List<NearbyAttraction> getNearbyAttractions(@RequestParam String userName) {
     	VisitedLocation visitedLocation = tourGuideService.getUserLocation(getUser(userName));
         return tourGuideService.getNearByAttractions(visitedLocation, getUser(userName));
     }
     
-    @RequestMapping("/getRewards") 
+    @GetMapping("/getRewards")
     public List<UserReward> getRewards(@RequestParam String userName) {
     	return tourGuideService.getUserRewards(getUser(userName));
     }
 
-    @RequestMapping("/getAllCurrentLocations")
+    @GetMapping("/getAllCurrentLocations")
     public HashMap<String, Location> getAllCurrentLocations() {
         HashMap<String, Location> allCurrentLocations = tourGuideService.getAllCurrentLocations();
         return allCurrentLocations;
     }
     
-    @RequestMapping("/getTripDeals")
+    @GetMapping("/getTripDeals")
     public List<Provider> getTripDeals(@RequestParam String userName) {
     	List<Provider> providers = tourGuideService.getTripDeals(getUser(userName));
         return providers;
     }
 
-    @RequestMapping("/getPreferences")
+    @GetMapping("/getPreferences")
     public UserPreferences getPreferences(@RequestParam String userName) {
         UserPreferences userPreferences = tourGuideService.getUserPreferences(getUser(userName));
         return userPreferences;
     }
 
-    @RequestMapping("/postPreferences")
+    @GetMapping("/postPreferences")
     public UserPreferences postPreferences(@RequestParam String userName, @RequestBody UserPreferences userPreferences) {
         return tourGuideService.postUserPreferences(getUser(userName), userPreferences);
     }
